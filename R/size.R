dafa2522ce34b2fb32de66ea12e4f14fea98a7ed
Giorgio Comai <giorgiocomai@gmail.com>
2017-01-25 11:14:11 +0100

    introduce ZotSize

---
+++ R/size.R
@@ -0,0 +1,23 @@
+#' Find size of zotero objects stored locally
+#'
+#' Find size of zotero objects stored locally
+#'
+#'
+#' @param path Path to local Zotero storage sub-folde, e.g. "/home/user/.mozilla/firefox/mdk7yirc.default/zotero/storage"
+#' @return A data.frame (tibble) with ID and size of each item present in the local storage.
+#' @export
+#' @examples
+#'
+#' size <- ZotSize()
+
+ZotSize <- function(path) {
+    itemFolders <- list.files(path = path, full.names = TRUE)
+    id <- sub(pattern = ".*storage/", replacement = "", x = itemFolders)
+    itemSize <- tibble::tibble(ID = id, Size = as.numeric(NA))
+    for (i in seq_along(itemFolders)) {
+        itemSize$Size[i] <- sum(file.info(list.files(path = itemFolders[i], all.files = TRUE, recursive = TRUE, full.names = TRUE))$size)
+    }
+    itemSize %>% arrange(desc(Size))
+}
+
+
