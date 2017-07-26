4408e92d272bd2c904e236ba3286e38cb1abf28c
Giorgio Comai <giorgiocomai@gmail.com>
2017-07-26 12:18:16 +0300

    introducing ZotReadChildren

--- R/read.R
+++ R/read.R
@@ -27,6 +27,35 @@ ZotReadItem <- function(id, user = NULL, credentials = NULL) {
     jsonlite::fromJSON(txt = paste0("https://api.zotero.org/users/", user, "/items/", id, "?key=", secret))
 }
 
+#' Read details of all children of a Zotero item
+#'
+#' Read details of all children of a Zotero item
+#'
+#'
+#' @param id Id code of a zotero item
+#' @param user Zotero userId
+#' @param credentials Either an R object created with AuthZot(store = TRUE), or an API secret key with write access created at https://www.zotero.org/settings/keys
+#' @return A list including all available details on a given item.
+#' @export
+#' @examples
+#'
+#' item <- ZotReadChildren()
+
+ZotReadChildren <- function(id, user = NULL, credentials = NULL) {
+    if (is.null(user) == TRUE) {
+        user <- ZotOptions("user")
+    }
+    if (is.null(credentials) == TRUE) {
+        credentials <- ZotOptions("credentials")
+    }
+    if (class(credentials)[1]=="OAuth") {
+        secret <- credentials$oauthSecret
+    } else {
+        secret <- credentials
+    }
+    jsonlite::fromJSON(txt = paste0("https://api.zotero.org/users/", user, "/items/", id, "/children", "?key=", secret), flatten = TRUE)
+}
+
 #' Extract collections in which given item is included
 #'
 #' Extract collections in which given item is included
