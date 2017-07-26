ccb65e07dd526c29cd26ad10e5be44b7a8f79981
Giorgio Comai <giorgiocomai@gmail.com>
2017-07-26 12:29:12 +0300

    Introducing ZotReadChildrenId

--- R/read.R
+++ R/read.R
@@ -35,7 +35,7 @@ ZotReadItem <- function(id, user = NULL, credentials = NULL) {
 #' @param id Id code of a zotero item
 #' @param user Zotero userId
 #' @param credentials Either an R object created with AuthZot(store = TRUE), or an API secret key with write access created at https://www.zotero.org/settings/keys
-#' @return A list including all available details on a given item.
+#' @return A data.frame including details on all children of a given parent item.
 #' @export
 #' @examples
 #'
@@ -54,6 +54,31 @@ ZotReadChildren <- function(id, user = NULL, credentials = NULL) {
         secret <- credentials
     }
     jsonlite::fromJSON(txt = paste0("https://api.zotero.org/users/", user, "/items/", id, "/children", "?key=", secret), flatten = TRUE)
+}
+
+
+#' Read IDs of all children of a Zotero item
+#'
+#' Read IDs of all children of a Zotero item
+#'
+#'
+#' @param id Id code of a zotero item
+#' @param user Zotero userId
+#' @param credentials Either an R object created with AuthZot(store = TRUE), or an API secret key with write access created at https://www.zotero.org/settings/keys
+#' @return A vector including the ID of all children
+#' @export
+#' @examples
+#'
+#' item <- ZotReadChildren()
+
+ZotReadChildrenId <- function(id, user = NULL, credentials = NULL) {
+    if (is.null(user) == TRUE) {
+        user <- ZotOptions("user")
+    }
+    if (is.null(credentials) == TRUE) {
+        credentials <- ZotOptions("credentials")
+    }
+    ZotReadChildren(id = id, user = user, credentials = credentials)$key
 }
 
 #' Extract collections in which given item is included
