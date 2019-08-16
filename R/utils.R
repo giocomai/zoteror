a2e71cc2514e30a899d8ae159968e65ec019e2a4
Giorgio Comai <giorgiocomai@gmail.com>
2019-08-16 19:50:03 +0200

    fix documentation

--- R/utils.R
+++ R/utils.R
@@ -3,15 +3,15 @@
 #' Transform a characther vectors of creators (authors, etc.) into a list of properly categorised data frames
 #'
 #'
-#' @param item_type Defaults to "book". It must correspond to a valid item type. You can chech which item types are valid with the function `zot_get_item_types()`
-#' @param cache Logical, defaults to TRUE. If TRUE, it stores the template in a "zot_cache" folder in the current working directory.
+#' @param creator A character vector (usually, a column in a data frame). Names must be given in the format: "Surname, Name; Surname, Name" (use semicolumn only to separate between multiple authors).
+#' @param creator_type Defaults to "author". Must be a character vector of length 1.
 #' @return A data frame, with one column for each accepted input for the given item type.
 #' @export
 #' @examples
 #'
 #' zot_convert_creators_to_df(creator = "De Gasperi, Alcide")
 
-zot_convert_creators_to_df <- function(creator) {
+zot_convert_creators_to_df <- function(creator, creator_type = "author") {
     purrr::map(.x = creator, .f = function(x) {
         temp_author_l <- purrr::map(.x = x,
                                     .f = function(y) stringr::str_split(string = y,
@@ -22,12 +22,12 @@ zot_convert_creators_to_df <- function(creator) {
                                       .f = tibble::as_tibble)
 
         if (ncol(temp_author)==1) {
-            tibble::tibble(creatorType = "author",
+            tibble::tibble(creatorType = creator_type,
                            firstName = "",
                            lastName = purrr::pluck(.x = temp_author, 1))
 
         } else {
-            tibble::tibble(creatorType = "author",
+            tibble::tibble(creatorType = creator_type,
                            firstName = purrr::pluck(.x = temp_author, 2),
                            lastName = purrr::pluck(.x = temp_author, 1))
 
